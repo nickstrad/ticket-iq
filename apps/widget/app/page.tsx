e73acdce97ed04ea554e13c109327cf8721b8fcc
Nick Strad <nickstrad.dev@gmail.com>
2025-08-14 21:57:10 -0500

    setup monorepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,4 +1,3 @@
-import { add } from "@workspace/math/add";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
@@ -6,7 +5,6 @@ export default function Page() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello app</h1>
-        <p>{`2 + 3 = ${add(2, 3)}`}</p>
         <Button size="sm">Button</Button>
       </div>
     </div>
