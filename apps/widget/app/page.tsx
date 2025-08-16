197224888ae4a726c8254d1caafe65ccdbe70f12
Nick Strad <nickstrad.dev@gmail.com>
2025-08-15 21:19:04 -0500

    Create convex package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@workspace/ui/components/button";
+"use client";
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app</h1>
-        <Button size="sm">Button</Button>
-      </div>
+      <p>apps/widget</p>
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
