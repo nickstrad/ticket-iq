956daf4aa11ddd71fe3085b6210b986bd422d940
Nick Strad <nickstrad.dev@gmail.com>
2025-08-17 09:55:52 -0500

    add clerk logic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,32 @@
 "use client";
+import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { useMutation, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <p>apps/web</p>
-      <Button type="button" onClick={() => addUser()}>
-        Add
-      </Button>
-      {JSON.stringify(users, null, 2)}
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <p>apps/web</p>
+          <UserButton />
+          <Button type="button" onClick={() => addUser()}>
+            Add
+          </Button>
+          {JSON.stringify(users, null, 2)}
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        Must be signed in! <SignInButton>Sign In</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import * as React from "react";
+import { ConvexReactClient } from "convex/react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error(
+    "NEXT_PUBLIC_CONVEX_URL is not defined. Please set it in your environment variables."
+  );
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
 }
