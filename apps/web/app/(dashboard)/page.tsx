febfc14dec76bf55699af84f40558c190c80dcd0
Nick Strad <nickstrad.dev@gmail.com>
2025-08-17 22:21:16 -0500

    Add organization logic

---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation, useQuery } from "convex/react";
+
+export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/web</p>
+      <UserButton />
+      <OrganizationSwitcher hidePersonal />
+      <Button type="button" onClick={() => addUser()}>
+        Add
+      </Button>
+      {JSON.stringify(users, null, 2)}
+    </div>
+  );
+}
